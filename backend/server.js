f0b10d5a93d129c1aa29d8a50deb887082e1a1d9
Peterxyjz <huyq09@gmail.com>
2024-07-19 01:25:54 +0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
@@ -7,19 +8,24 @@ import connectToMongoDB from "./services/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js"
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-//home server
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+
 //routes:
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
