01e2acd8d9cd51a5ba5baefd1535c30b39dd37ac
Peterxyjz <huyq09@gmail.com>
2024-07-19 01:42:24 +0700

    update socket io

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -13,7 +13,11 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      // const socket = io("http://localhost:5000", {
+      //   query: { userId: authUser._id },
+      // });
+      
+      const socket = io("https://free-chat-sikd.onrender.com", {
         query: { userId: authUser._id },
       });
 
